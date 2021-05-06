53762cd6b3a74c39fb4647758d4376c50ecdfbb9
AVANADE-CORP\w.oliveira.carvalho <w.oliveira.carvalho@avanade.com>
2021-05-06 15:59:30 -0300

    Starter Project

---
+++ src/test/java/br/com/consultarEndereco/ConsultarEnderecoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.consultarEndereco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultarEnderecoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
